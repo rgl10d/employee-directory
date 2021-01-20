2ec80ac85e8ed145053730b8d08fc0f82b49cb0b
Garrett Lee <rgl10d@gmail.com>
2021-01-20 16:13:34 -0500

    initial file push

---
+++ src/containers/Directory/Directory.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Directory extends Component {
+    render() {
+        return (
+            <div>
+                This is the home/directory page.
+            </div>
+        );
+    }
+}
+
+export default Directory;
