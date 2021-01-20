cadd3fe179ca00eacbe82777de3c6bab8765ef07
Garrett Lee <rgl10d@gmail.com>
2021-01-20 17:21:40 -0500

    successful api call console logged

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -6,10 +6,10 @@ const styles = {
     }
 }
 
-const EmployeeCard = () => {
+const EmployeeCard = (props) => {
     return (
         <div className="card" style={styles.cardWidth}>
-        <img src="..." class="card-img-top" alt="..."/>
+        <img src="..." className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- src/containers/Directory/Directory.jsx
+++ src/containers/Directory/Directory.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import axios from "axios";
 import EmployeeCard from "../../components/EmployeeCard/EmployeeCard";
 import Wrapper from "../../components/Wrapper/Wrapper";
 
 class Directory extends Component {
+  componentDidMount() {
+    this.generateEmployees();
+  }
+
+  generateEmployees = () => {
+    axios
+      .get("https://randomuser.me/api/")
+      .then((response) => {
+        console.log(response.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div className="container">
